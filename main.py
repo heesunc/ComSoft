e5e69def3ea0f5670b97ee49b06e5b4308c03be7
MaroHan77 <callmeyami7@gmail.com>
2023-11-16 12:00:43 +0900

    숫자가 짝수인지 확인하는 함수 - 마로

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def is_even(number):
+    return number % 2 == 0
+
+# 예시 사용
+your_number = 4
+if is_even(your_number):
+    print(f"{your_number}은(는) 짝수입니다.")
+else:
+    print(f"{your_number}은(는) 홀수입니다.")
